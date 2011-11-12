e433bfdfc65c9500710e686df4893a0867b5624f
Sarah Supp <sarah@weecology.org>
2011-11-12 22:37:22 +0000

    Added code to allow access to the database on the server. Do I need an extra piece of code to tell it what table I'm using?

--- Weather_python_sql.py
+++ Weather_python_sql.py
@@ -1,6 +1,8 @@
 # Weather Data for Portal
 import numpy as np
 import calendar
+from datetime import datetime as dt
+from matplotlib.dates import num2date
 import MySQLdb as dbapi
 
 """What needs to happen when entering WEATHER data:
@@ -25,6 +27,7 @@ def jday2caldates(data_line):
         cal_day = days[int(data_line[2]) - 1]
         cal_month = months[int(data_line[2]) - 1]
         data_line.extend([cal_month, cal_day])
+        data_line[0] = int(data_line[0])
         return data_line
     else :
         days = sum([range(1,32), range(1,29), range(1,32), range(1,31), range(1,32), range(1,31),range(1,32), range(1,32), range(1,31),range(1,32), range(1,31),range(1,32)],[])
@@ -32,9 +35,9 @@ def jday2caldates(data_line):
         cal_day = days[int(data_line[2]) - 1]
         cal_month = months[int(data_line[2]) - 1]
         data_line.extend([cal_month, cal_day])
+        data_line[0] = int(data_line[0])
         return data_line
 
-    
 def rearrange_cols(data_line):
     '''where order of data_line is: 
     array, year, julianDay, hour, ppt, tempAir, relHumid, tempSoil, month, day
@@ -45,7 +48,7 @@ def rearrange_cols(data_line):
     data_line = [data_line[i] for i in myorder]
     return data_line
     
-def prepare_data(data_line):
+def prepare_data_row(data_line):
     '''inputs a line of data from weather.dat and determines if it's a battery reading.
     If not, then  the dataline is manipulated and appended to fit the Portal weather database.'''
     add_tempSoil(data_line)
@@ -61,7 +64,7 @@ def compile_weather_data(data):
     list of weather data to be appended to the database.'''
     weather_data = []
     for line in weather:
-        wx = prepare_data(line)
+        wx = prepare_data_row(line)
         weather_data.append(wx)
     return weather_data
             
@@ -78,33 +81,33 @@ def save_weather_file(data, filename):
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% DOES STUFF
 #location of new weather file
 filename = input('Where is your most recent weather file located? ' )
-#'F:\AdvProj_Portal\Met132.dat'
-filename = ('F:\AdvProj_Portal\Met395.DAT')
+#'F:\AdvProj_Portal\Met395.dat'
 
 # import into python
-# elements in the list:  
-# 0 = array, 1 = year, 2 = Julian day, 3 = hour, 4 = ppt, 5 = tempAir, 6 = RelHumid
+''' elements in the list:  
+0 = array, 1 = year, 2 = Julian day, 3 = hour, 4 = ppt, 5 = tempAir, 6 = RelHumid 
+The length check makes sure NOT to import shorter rows, which are battery readings.'''
 datafile = open(filename, 'r')
 weather = []
 for row in datafile:
     row_data = row.strip().split(',')
-    if len(row_data) == 7:  # don't import battery readings
+    if len(row_data) == 7:
         row_data = map(float, row_data)
         weather.append(row_data)
         
-
 # get data to be appended to database
 weather_to_add = compile_weather_data(weather)
 
 
 #DATABASE STUFF: open file to append to database
-database = 'loc/weather_db.sqlite'
-server_login = """host = 'serenity.bluezone.usu.edu',
-                    port = 1995,
-                    user = sarahsupp,
-                    passwd = yourpassword"""
+user = input('What is your username?: ')
+yourpassword = input('Please enter your password: ')
 
-con = dbapi.connect(server_login)
+con = dbapi.connect(host = 'serenity.bluezone.usu.edu',
+              port = 1995,
+              user = user,
+              passwd = yourpassword)
+
 cur = con.cursor()
 
 cur.execute(USE weather) #or whatever the name of portal weather database is
@@ -126,11 +129,18 @@ FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 IGNORE 0 LINES""")
 con.commit()
 
-#append data to Hourly weather Table
+#append data to Hourly weather Table, making sure not to repeat data that already exists
 cur.execute("INSERT INTO Portal.Rodents SELECT newdat.* FROM queries.newdat")
 con.commit()
 
 # run a query to add daily data to 'Daily' table in database 
+cur.execute("""SELECT DISTINCTROW [Hourly (mm)].Year, [Hourly (mm)].Month, [Hourly (mm)].Day, 
+Avg([Hourly (mm)].TempAir) AS TempAirAvg, Max([Hourly (mm)].TempAir) AS TempAirMax, 
+Min([Hourly (mm)].TempAir) AS TempAirMin, Avg([Hourly (mm)].RelHumid) AS [RH Avg], 
+Max([Hourly (mm)].RelHumid) AS [RH Max], Min([Hourly (mm)].RelHumid) AS [RH Min], 
+Avg([Hourly (mm)].TempSoil) AS TempSoilAvg, Max([Hourly (mm)].TempSoil) AS TempSoilMax, Min([Hourly (mm)].TempSoil) AS TempSoilMin, Sum([Hourly (mm)].[Precipitation (mm)]) AS [Total Precipitation] INTO [Daily (mm)]
+FROM [Hourly (mm)]
+GROUP BY [Hourly (mm)].Year, [Hourly (mm)].Month, [Hourly (mm)].Day;""")
 
 
 # run a query to add monthly data to 'Monthly table in database
